7bd2afe1e1875c72dacca4af24fa62c317234cac
alscjf4075 <alscjf4075@naver.com>
2021-03-25 21:34:45 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 module.exports = router;    //라우터를 사용할수있게 외부로 내보내줌
