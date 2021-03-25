65be20e6e5ee62f0828d355d54d6c9d2fa991e56
alscjf4075 <alscjf4075@naver.com>
2021-03-26 04:19:24 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -2,7 +2,19 @@
 
 const fs = require("fs").promises;   //users.json에 접근을해서 읽어올 수 있도록해줌 파일시스템
 
-class UserStorage {    
+class UserStorage {   
+    static #getUserInfo(data, id){      //프라이빗한 변수나 메서드는 항상 최상단에 올려줌
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        
+        return userInfo;
+    } 
+
     static getUsers(...fields){
         // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
@@ -21,18 +33,6 @@ class UserStorage {
                 return this.#getUserInfo(data, id);     //은닉화 된 메서드 호출 getUserInfo랑 다름
             })     
             .catch(console.error);   //실패
-    }
-
-    static #getUserInfo(data, id){
-        const users = JSON.parse(data);
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-        
-        return userInfo;
     }
 
     static save(userInfo){
